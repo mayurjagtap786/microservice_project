9548f58071d72a49856eb4a0a41280a82bbb600a
mayurjagtap786 <mayurjagtap786@gmail.com>
2026-01-26 23:42:53 +0530

    inventory service fixes

--- inventory-servcie/src/main/java/org/shopping/kart/inventory/exception/InsufficientStockException.java
+++
@@ -1,8 +0,0 @@
-package org.shopping.kart.inventory.exception;
-
-public class InsufficientStockException extends RuntimeException{
-
-    public InsufficientStockException(Long productId){
-        super(String.format("Insufficient Stock %d",productId));
-    }
-}
